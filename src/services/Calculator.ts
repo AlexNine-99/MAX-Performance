6e80646e28695c7b90c06496c9a8e8ba263554ca
AlexNine-99 <adoty1425@gmail.com>
2025-02-15 13:00:00 -0800

    cleared branch

--- src/services/Calculator.ts
+++
@@ -1,8 +0,0 @@
-export type CalculatorOutput =
-  | { type: 'result'; value: string }
-  | { type: 'error'; inputId: string; message: string }
-
-export default interface Calculator {
-  inputIds: Set<string>
-  calculate(inputValues: Map<string, string>): Map<string, CalculatorOutput>
-}
--- src/services/createRegistryType.ts
+++
@@ -1,5 +0,0 @@
-import { createSafeMapType } from './SafeMap'
-
-export default function createRegistryType<T>(itemDescriptor: string) {
-  return createSafeMapType<string, T>('id', itemDescriptor)
-}
