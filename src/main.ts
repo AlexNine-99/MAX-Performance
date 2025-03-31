112c7361870cb9611e32d3f9c91a059bd95848fc
AlexNine-99 <adoty1425@gmail.com>
2025-03-30 22:43:05 -0700

    Concisened app setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import 'bulma'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+createApp(App).use(createPinia()).mount('#app')
